b1901cf10aa941fb824ebec954e2d6a67acc6f89
marco-scocca <marcoscocca1991@gmail.com>
2024-07-03 06:54:14 +0200

    finish email

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-let email = ['marco@mail.it', 'marco@mail.it'];
+let emails = ['marco@mail.it', 'francesco@mail.it'];
 const btn = document.getElementById('check');
+
 btn.addEventListener('click', function () {
 
     let user_email = document.getElementById('email').value;
+    let check_mail = false;
 
 
+    for (let i = 0; i < emails.lenght; i++) {
+        if (emails[i] == user_email) {
+            check_mail = true;
+        }
+    }
+
+    if (check_mail) {
+        console.log('Mail trovata benvenuto');
+    }
+    else {
+        console.log('Mail non trovata');
+
+    }
 })
