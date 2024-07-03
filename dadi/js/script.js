f163143b8c2993afcb52d81ebb00b78617b82cb6
marco-scocca <marcoscocca1991@gmail.com>
2024-07-03 07:03:11 +0200

    confronto i due numeri

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,9 @@
 let user_number = Math.floor(Math.random() * 6 + 1);
 let pc_number = Math.floor(Math.random() * 6 + 1);
+
+if (user_number > pc_number) {
+    console.log("hai vinto")
+}
+else if (user_number < pc_number) {
+    console.log("hai perso")
+}
